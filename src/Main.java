b7b981df4be61c1502d9a99c8980afed927f6d4d
Yuliya Torhan <yuriss@unit-880.labs.intellij.net>
2016-06-29 15:16:47 +0300

    do nothing

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        doNothing();
+
         System.out.println("Hello World!");
+    }
+
+    private static void doNothing() {
+        // todo
     }
 }
