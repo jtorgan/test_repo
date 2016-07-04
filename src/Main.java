2ba18d546f18a664dc645e441f66b2ca02566a78
Yuliya Torhan <Yuliya.Torhan@jetbrains.com>
2016-07-04 17:17:22 +0300

    user1: change main

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,11 @@ public class Main {
     public static void print() {
         System.out.println("Hello World 2!");
     }
+    public static void print1() {
+        System.out.println("Hello World 2!");
+    }
+    public static void print2() {
+        System.out.println("Hello World 2!");
+    }
 
 }
