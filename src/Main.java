f16da43ee625febf0e58ca6d2748b87472b29a85
Yuliya Torhan <Yuliya.Torhan@jetbrains.com>
2016-07-04 16:56:41 +0300

    change config

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+        System.out.println("Hello World!");
+    }
+
+    public static void print() {
+        System.out.println("Hello World 2!");
     }
 }
